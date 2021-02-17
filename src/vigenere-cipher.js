26ec1dc944a5ff50dae7fc922f8e324d69eabba4
Loktysh Alexandr <vebster24@mail.ru>
2021-02-17 21:31:10 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
   }
+  
+  encrypt(message1, keyy) {
+      if (arguments[0] === undefined || arguments[1] === undefined) throw new Error();
+      const { toUpper } = require("lodash");
+      function mod(n, m) {return ((n % m) + m) % m;}
+      let message = toUpper(message1).split('');
+      let key = toUpper(keyy);
+      let key_max = '';
+      let res = [];
+      while (key_max.length < message.length) {
+          key_max += key
+      }
+      for (let i = 0, i1 = 0; i < message.length; i++, i1++) {
+          let el_m = message[i]; 
+          let el_k = key_max[i1];
+          if (el_m.charCodeAt(0) < 65  || el_m.charCodeAt(0) > 90) {
+              res.push(el_m)
+              i1--
+          }
+          else {
+              let a = ((el_m.charCodeAt(0))); 
+              let b = ((el_k.charCodeAt(0)));
+              res.push(String.fromCharCode(mod(a+b, 26)+65))
+          }
+      }
+      if (this.mode === true) return res.join('');
+      return res.reverse().join('');
+  }    
+  decrypt(message1, keyy) {
+    if (arguments[0] === undefined || arguments[1] === undefined) throw new Error();
+    const { toUpper } = require("lodash");
+    function mod(n, m) {return ((n % m) + m) % m;}
+    let message = toUpper(message1).split('')
+    let key = toUpper(keyy)
+    let key_max = ''
+    let res = []
+    while (key_max.length < message.length) {
+        key_max += key
+    }
+    for (let i = 0, i1 = 0; i < message.length; i++, i1++) {
+        let el_m = message[i]; 
+        let el_k = key_max[i1];
+        if (el_m.charCodeAt(0) < 65  || el_m.charCodeAt(0) > 90) {
+            res.push(el_m)
+            i1--
+        }
+        else {
+            let a = ((el_m.charCodeAt(0))); 
+            let b = ((el_k.charCodeAt(0)));
+            res.push(String.fromCharCode(mod(a+26-b, 26)+65))
+        }
+    }
+    if (this.mode === true) return res.join('');
+    return res.reverse().join('');
+  } 
 }
 
 module.exports = VigenereCipheringMachine;
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 // require('./test/count-cats.test'); done
-require('./test/carbon-dating.test');
+// require('./test/carbon-dating.test'); done
 // require('./test/dream-team.test'); done
 // require('./test/extended-repeater.test'); done
 // require('./test/what-season.test'); done
-// require('./test/vigenere-cipher.test');
+require('./test/vigenere-cipher.test');
 // require('./test/hanoi-tower.test'); done
 // require('./test/transform-array.test'); done
 // require('./test/simple-chain.test');
