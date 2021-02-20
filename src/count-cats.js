951a3bc92df1ecb139e8520dee3bd38648f7f966
Loktysh Alexandr <vebster24@mail.ru>
2021-02-20 19:58:08 +0300

    fix: improver count-cats algorithm

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  return matrix.flat().filter((e) => e === '^^').length;
+  // return matrix.flat().filter((e) => e === '^^').length;
+  let res = 0
+  for (let item in matrix) {
+      matrix[item].map( c => c === '^^' ? res++ : res)
+  };
+  return res
 };
